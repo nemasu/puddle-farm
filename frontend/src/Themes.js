cd1f5d812646d3956e7f1cbbe8b1cf14d0d4a2fe
MaxG4 <test@maxg4.dev>
2024-10-07 12:18:47 +0200

    new themes for Bedman and Nagoriyuki

--- frontend/src/Themes.js
+++ frontend/src/Themes.js
@@ -48,50 +48,143 @@ Themes.set('Sol', createTheme({
     },
   }));
 
-  Themes.set('Ky', createTheme({
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#217DBB',
-      },
-      secondary: {
-        main: '#DDDBD5',
-      },
-      background: {
-        default: '#282E30',
-      },
+
+Themes.set('Ky', createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#217DBB',
     },
-    typography: {
-        pageHeader: {
-            display: 'block',
-            fontSize: 34,
-            color: '#217DBB',
-        },
+    secondary: {
+      main: '#DDDBD5',
     },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            variants: [
-              {
-                props: { variant: 'link'},
-                style: {
-                  textTransform: 'none',
-                  color: '#F8B552',
-                }
+    background: {
+      default: '#282E30',
+    },
+  },
+  typography: {
+    pageHeader: {
+      display: 'block',
+      fontSize: 34,
+      color: '#217DBB',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { variant: 'link'},
+              style: {
+                textTransform: 'none',
+                color: '#F8B552',
               }
-            ],
-          },
-        },
-      },
-      MuiAppBar: {
-        styleOverrides: {
-          root: {
-            backgroundColor: '#217DBB',
-          },
+            }
+          ],
         },
       },
     },
-  }));
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#217DBB',
+        },
+      },
+    },
+  },
+}));
+
+Themes.set('Nagoriyuki', createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#60304f',
+    },
+    secondary: {
+      main: '#DDDBD5',
+    },
+    background: {
+      default: '#282E30',
+    },
+  },
+  typography: {
+    pageHeader: {
+      display: 'block',
+      fontSize: 34,
+      color: '#da2a46',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { variant: 'link'},
+              style: {
+                textTransform: 'none',
+                color: '#da2a46',
+              }
+            }
+          ],
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#217DBB',
+        },
+      },
+    },
+  },
+}));
+
+Themes.set('Bedman?', createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#471d37',
+    },
+    secondary: {
+      main: '#a83b5e',
+    },
+    background: {
+      default: '#171717',
+    },
+  },
+  typography: {
+    pageHeader: {
+      display: 'block',
+      fontSize: 34,
+      color: 'white',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { variant: 'link'},
+              style: {
+                textTransform: 'none',
+                color: '#F8B552',
+              }
+            }
+          ],
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#471d37',
+        },
+      },
+    },
+  },
+}));
 
   export default Themes;
