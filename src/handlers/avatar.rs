3a6f368098025ca4c8b507cf934a471a73b6e289
nemasu <nemasu@gmail.com>
2025-01-11 00:28:57 +0900

    Fix avatar colors.

--- src/handlers/avatar.rs
+++ src/handlers/avatar.rs
@@ -1,5 +1,14 @@
 use axum::body::Bytes;
 
+fn process_channel(value: u8) -> u8 {
+    if value != 0 && value != 255 {
+        let n = f64::sqrt(value as f64 / 256.0);
+        (n * 256.0) as u8
+    } else {
+        value
+    }
+}
+
 pub async fn handle_get_avatar(png: String) -> Bytes {
     // Decode base64
     let png_bytes = base64_url::decode(&png).unwrap();
@@ -10,8 +19,11 @@ pub async fn handle_get_avatar(png: String) -> Bytes {
     // Convert to RGBA
     let mut rgba = img.to_rgba8();
 
-    // Invert alpha channel
+    // Process image
     for pixel in rgba.pixels_mut() {
+        pixel[0] = process_channel(pixel[0]);
+        pixel[1] = process_channel(pixel[1]);
+        pixel[2] = process_channel(pixel[2]);
         pixel[3] = 255 - pixel[3];
     }
 
